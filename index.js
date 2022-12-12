f4107912ebb034353ca9b61b990211f78609162f
mhkeller <code@mhkeller.com>
2022-12-12 18:46:29 -0500

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+import module from './my-module.js'
+
+for (let i = 0; i < 1_000_000; i += 1) {
+	console.log(i);
+}
---
+++ my-module.js
@@ -0,0 +1,6 @@
+// Uncomment this line and control-c will stop working
+import { chromium } from 'playwright';
+
+export default function myModule() {
+
+}
